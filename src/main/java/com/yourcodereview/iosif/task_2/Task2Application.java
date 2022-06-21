02ae1d5530988fdea0253acd66133ad23d896d83
Iosif Futerman <ifuterman@yandex.ru>
2022-06-21 20:32:23 +0500

    File database included

--- src/main/java/com/yourcodereview/iosif/task_2/Task2Application.java
+++ src/main/java/com/yourcodereview/iosif/task_2/Task2Application.java
@@ -2,9 +2,7 @@ package com.yourcodereview.iosif.task_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
